483d447359b9962102b92fd8ba5f98e5328748b6
galyagenova4 <galyagenova4@gmail.com>
2017-06-14 15:52:51 +0200

    modifiche sull app.js ed il controller del profilo aggiunta la factory

--- profilo/profilo.controllers.js
+++ profilo/profilo.controllers.js
@@ -1,8 +1,13 @@
-app.controller('ProfiloCtrl', [ '$profiloService', '$state', function($profiloService, $state ){
+app.controller('ProfiloCtrl', [ '$profiloFactory','$profiloService', '$state', function($profiloFactory, $profiloService, $state ){
     var self=this;
     self.profiloData= function(){
         $profiloService.profilo().then(function(result){
             self.profilo=result.data;
-        })
-    }
+            $profiloFactory.nome = self.profilo.nome;
+            $profiloFactory.cognome = self.profilo.cognome;
+            $profiloFactory.dataUltimoAccesso = self.profilo.dataUltimoAccesso;
+            $profiloFactory.codiceFiscale = self.profilo.codiceFiscale;
+            $profiloFactory.indirizzo = self.profilo.indirizzo;
+        });
+    };
 }]);
