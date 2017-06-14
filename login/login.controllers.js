14f54140e72d3cdc927b1d7c240790dc5c51742e
Tullio <t.fornersi94@gmail.com>
2017-06-14 14:30:52 +0200

    token in local storage

--- login/login.controllers.js
+++ login/login.controllers.js
@@ -1,8 +1,10 @@
-app.controller('LoginCtrl', ['$loginService', '$state', function ($loginService, $state) {
+app.controller('LoginCtrl', ['$loginService', '$state', '$scope', function ($loginService, $state, $scope) {
     var self = this;
     self.login = function (username, password) {
         $loginService.logga(username, password).then(function (result) {
-            self.loginToken = result.data;
+            self.loginToken = result.data.token;
+            console.log(self.loginToken);
+            localStorage.setItem('tokenJwt', self.loginToken);
             $state.go('dashboard');
         }).catch(function (error) {
             console.log('errore nel login: ', error);
