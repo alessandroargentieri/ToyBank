bde0dd9434313991a8817fe503962d99855186ed
galyagenova4 <galyagenova4@gmail.com>
2017-06-14 18:13:10 +0200

    some changes

--- bonifico/bonifico.controllers.js
+++ bonifico/bonifico.controllers.js
@@ -2,7 +2,9 @@ app.controller('BonificoCtrl', ['$bonificoService'], function($bonificoService) 
 
     var self = this;
     self.bonifico = function() {
-        $bonificoService.bonifico(self.nome, self.cognome, self.iban, self.importo,self.data, self.causale)
-    }
+        $bonificoService.bonifico(self.nome, self.cognome, self.iban, self.importo,self.data, self.causale).then(function(result){
+            /*in result c'è il bonifico completo e la key che servirà per verificare l'OTP*/
+        });
+    };
 
 });
