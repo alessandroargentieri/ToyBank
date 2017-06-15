21bba50f1c0e6ceecca61834c5065b1152b0b3e3
Chiara <marc_chiara@yahoo.it>
2017-06-15 09:48:43 +0200

    modificato il service del bonifico

--- bonifico/bonifico.controllers.js
+++ bonifico/bonifico.controllers.js
@@ -1,8 +1,10 @@
 app.controller('BonificoCtrl', ['$bonificoService'], function($bonificoService) {
 
     var self = this;
-    self.bonifico = function() {
-        $bonificoService.bonifico(self.nome, self.cognome, self.iban, self.importo,self.data, self.causale)
-    }
+    self.richiestaBonificoCtrl = function() {
+        $bonificoService.richiestaBonifico(self.nome, self.cognome, self.iban, self.importo,self.data, self.causale).then(function(result){
+            self.bonifico=result.data;
+        });
+    };
 
 });
